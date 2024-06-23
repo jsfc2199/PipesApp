f8513a9d97bb88e9d27d6f79c71ab8f13f056543
Juanse Franco <jsfc2199@gmail.com>
2024-06-23 12:50:50 -0500

    navbar usando prime ng

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
-
+import { MenubarModule } from 'primeng/menubar';
 @NgModule({
-  exports: [MenuModule]
+  exports: [MenuModule, MenubarModule]
 })
 export class PrimeNgModule { }
