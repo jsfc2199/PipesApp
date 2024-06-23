c6e8ed80dd823b03ebc6d338ccc66be1c9d31f90
Juanse Franco <jsfc2199@gmail.com>
2024-06-23 14:19:01 -0500

    Uso del i18plural pipe

--- src/app/products/pages/common-page/common-page.component.ts
+++ src/app/products/pages/common-page/common-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-common-page',
   templateUrl: './common-page.component.html',
-  styleUrl: './common-page.component.css'
+  styleUrl: './common-page.component.css',
 })
 export class CommonPageComponent {
   //i18n Select
@@ -11,12 +11,33 @@ export class CommonPageComponent {
   public gender: 'male' | 'female' = 'male';
 
   invitationMap = {
-    'male':'invitarlo',
-    'female': 'invitarla'
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    this.name = 'Mellisa';
+    this.gender = 'female';
   }
 
-  changeClient(){
-    this.name = 'Mellisa'
-    this.gender = 'female'
+  //i18plural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+
+  deleteClient(){
+    this.clients.shift()
+  }
+
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
   }
 }
